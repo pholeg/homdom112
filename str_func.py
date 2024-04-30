3646b41a1d0d2ea75a2da54a65d277b54be21069
ph_oleg <ph_oleg@mail.ru>
2024-04-30 15:09:25 +0300

    подобавляли всякого для выполнения домашки

--- str_func.py
+++ str_func.py
@@ -1,2 +1,9 @@
 def caps_look(a):
+    """тута новая докстринга для конфликта весрий"""
+
     return a.upper()
+
+
+def upper_first_sumbol(a):
+    """эта типа докстринга - тута мы делаем титл в словах"""
+    return a.title()
